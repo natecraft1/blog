b9625de1ffcc76052898c34697199164fa47fd4e
Nate <nglass1988@gmail.com>
2013-07-21 15:33:03 -0700

    what

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 
 describe "Blogs" do
   describe "GET /blogs" do
-    it "works! (now write some real specs)" do
+    it "should display a login link" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get blogs_path
-      response.status.should be(200)
+      
     end
   end
 end
